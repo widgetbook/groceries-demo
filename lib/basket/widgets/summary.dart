9cc5ad2dc3f016c95ba0082138fcce26fd2b7b32
Jens Horstmann <jens@widgetbook.io>
2023-07-11 15:02:34 +0200

    chore: add knobs to Summary

--- lib/basket/widgets/summary.dart
+++ lib/basket/widgets/summary.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:groceries_app/theme/app_theme.dart';
+import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
 @UseCase(
@@ -11,10 +12,25 @@ import 'package:widgetbook_annotation/widgetbook_annotation.dart';
       'https://www.figma.com/file/EXuEpwiyksLAejYX1qr1v4/Demo-App-featuring-variables?type=design&node-id=112-6510&mode=dev',
 )
 Widget buildSummaryUseCase(BuildContext context) {
-  return const Summary(
-    total: 10,
-    delivery: 2,
-    subTotal: 8,
+  return Summary(
+    total: context.knobs.double.slider(
+      label: 'Total',
+      initialValue: 10,
+      min: 0,
+      max: 1000,
+    ),
+    delivery: context.knobs.double.slider(
+      label: 'Delivery',
+      initialValue: 2,
+      min: 0,
+      max: 10,
+    ),
+    subTotal: context.knobs.double.slider(
+      label: 'Subtotal',
+      initialValue: 2,
+      min: 0,
+      max: 1000,
+    ),
   );
 }
 
