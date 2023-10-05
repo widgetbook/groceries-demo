36e983ca90c607472310b195a04e30011d3e7c87
Roaa94 <roaa94khaddam@gmail.com>
2023-10-05 14:14:24 +0200

    feat: adds app-bar design link

--- lib/core/app_bar.dart
+++ lib/core/app_bar.dart
@@ -7,7 +7,12 @@ import 'package:groceries_app/theme/theme.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
-@UseCase(name: 'Default', type: AppBar)
+@UseCase(
+  name: 'Default',
+  type: AppBar,
+  designLink:
+      'https://www.figma.com/file/EXuEpwiyksLAejYX1qr1v4/Demo-App-featuring-variables?type=design&node-id=277-3056&mode=design&t=xEVe42MecBrfe39j-4',
+)
 Widget appBarUseCase(BuildContext context) {
   return AppBar(
     title: context.knobs.string(
