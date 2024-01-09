1e31b3f7ca9586ffddf84f88b7c533fe1c6ff238
tim <obidnyak@me.com>
2024-01-09 14:16:24 +0100

    test

--- widgetbook/lib/core/card.dart
+++ widgetbook/lib/core/card.dart
@@ -14,7 +14,7 @@ Widget card(BuildContext context) {
     child: Padding(
       padding: const EdgeInsets.all(8),
       child: Text(
-        'This is some text 102',
+        'This is some text 103',
         style: AppTheme.of(context).typography.bodyMedium16,
       ),
     ),
