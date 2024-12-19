7e7f75cd737a7d9596669e0c56f100547a3352b7
vukasin-petkovic-greenstate <vukasin.petkovic@greenstate.ch>
2024-12-19 15:03:40 +0100

    Update primary_button.dart

--- lib/ui/widgets/primary_button.dart
+++ lib/ui/widgets/primary_button.dart
@@ -24,11 +24,7 @@ class PrimaryButton extends StatelessWidget {
       decoration: BoxDecoration(
         color: AppTheme.of(context).background.brand,
         borderRadius: BorderRadius.circular(
-<<<<<<< Updated upstream
-          AppTheme.of(context).radius.full,
-=======
           1,
->>>>>>> Stashed changes
         ),
       ),
       child: Row(
