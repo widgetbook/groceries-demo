7e29f66f0da710c85bd686d3d9e92eee1c171f7d
Youssef Andrawes <YoussefRaafatNasry@gmail.com>
2025-02-26 00:49:14 +0100

    refactor: add some paddings

--- widgetbook/lib/ui/widgets/empty_state.dart
+++ widgetbook/lib/ui/widgets/empty_state.dart
@@ -11,12 +11,18 @@ import 'package:widgetbook_annotation/widgetbook_annotation.dart';
       'https://www.figma.com/design/HsANkdhbsCNTkXBzNJRNLD/Groceries-Demo?node-id=6809-5957&t=N6qwmLP7MP59ClWB-4',
 )
 Widget buildEmptyStateUseCase(BuildContext context) {
-  return EmptyState(
-    icon: FontAwesomeIcons.triangleExclamation,
-    title: context.knobs.string(label: 'title', initialValue: 'Title'),
-    message: context.knobs.string(
-      label: 'message',
-      initialValue: 'A message conveying the state of the product',
+  return Padding(
+    padding: const EdgeInsets.all(16),
+    child: EmptyState(
+      icon: FontAwesomeIcons.triangleExclamation,
+      title: context.knobs.string(
+        label: 'title',
+        initialValue: 'Title',
+      ),
+      message: context.knobs.string(
+        label: 'message',
+        initialValue: 'A detailed message conveying the state',
+      ),
     ),
   );
 }
