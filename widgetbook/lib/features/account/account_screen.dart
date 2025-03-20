09002fcd12337aa5e5453f3df8cec1dd379f5eb4
Youssef Andrawes <YoussefRaafatNasry@gmail.com>
2025-03-20 15:49:33 +0100

    fix account screen knobs

--- widgetbook/lib/features/account/account_screen.dart
+++ widgetbook/lib/features/account/account_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:groceries_app/data/mocks.dart';
+import 'package:groceries_app/data/data.dart';
 import 'package:groceries_app/features/features.dart';
 import 'package:groceries_app/root/root.dart';
 import 'package:widgetbook/widgetbook.dart';
@@ -13,9 +13,24 @@ Widget buildAccountScreenUseCase(BuildContext context) {
   return ProviderScope(
     overrides: [
       userProvider.overrideWith((ref) {
-        final state = context.knobs.viewState();
+        final state = context.knobs.viewState(
+          allowedValues: [
+            ViewState.data,
+            ViewState.loading,
+            ViewState.error,
+          ],
+        );
+
         return state.map(
-          data: MockDataStore.user,
+          data: User(
+            name: context.knobs.string(
+              label: 'name',
+              initialValue: 'John Doe',
+            ),
+            isVerified: context.knobs.boolean(
+              label: 'isVerified',
+            ),
+          ),
         );
       }),
     ],
