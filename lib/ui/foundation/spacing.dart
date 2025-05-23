a694e845edc5133a06a611d01f8dcf122d2a5e1c
yousinix <youssefraafatnasry@gmail.com>
2025-05-23 15:36:31 +0200

    refactor: update xs spacing

--- lib/ui/foundation/spacing.dart
+++ lib/ui/foundation/spacing.dart
@@ -1,7 +1,7 @@
 class DesignSystemSpacing {
   static const double zero = 0;
   static const double xxs = 4;
-  static const double xs = 8;
+  static const double xs = 10;
   static const double sm = 12;
   static const double m = 16;
   static const double l = 24;
