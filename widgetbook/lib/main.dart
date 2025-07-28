0b17f11c7e133ec70de4ed580e94750b361b5400
Youssef Andrawes <YoussefRaafatNasry@gmail.com>
2025-07-28 14:52:16 +0200

    reduce addons

--- widgetbook/lib/main.dart
+++ widgetbook/lib/main.dart
@@ -14,22 +14,8 @@ void main() {
 
 @App(
   cloudAddonsConfigs: {
-    'German Light': [
-      LocalizationAddonConfig('de'),
-      ThemeAddonConfig('Light'),
-    ],
-    'German Dark': [
-      LocalizationAddonConfig('de'),
-      ThemeAddonConfig('Dark'),
-    ],
-    'English Light': [
-      LocalizationAddonConfig('en'),
-      ThemeAddonConfig('Light'),
-    ],
-    'English Dark': [
-      LocalizationAddonConfig('en'),
-      ThemeAddonConfig('Dark'),
-    ],
+    'Dark': [ThemeAddonConfig('Dark')],
+    'Light': [ThemeAddonConfig('Light')],
   },
 )
 class WidgetbookApp extends StatelessWidget {
@@ -39,10 +25,8 @@ class WidgetbookApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Widgetbook(
       directories: directories,
-      appBuilder: (context, child) => ColoredBox(
-        color: const Color(0xFF404040),
-        child: child,
-      ),
+      appBuilder: (context, child) =>
+          ColoredBox(color: const Color(0xFF404040), child: child),
       addons: [
         BuilderAddon(
           name: 'Disable Animations (Cloud)',
@@ -59,10 +43,7 @@ class WidgetbookApp extends StatelessWidget {
         ),
         GitHubAddon('widgetbook/groceries-demo'),
         DeviceFrameAddon(
-          devices: [
-            Devices.ios.iPhone13,
-            Devices.ios.iPad,
-          ],
+          devices: [Devices.ios.iPhone13, Devices.ios.iPad],
           initialDevice: Devices.ios.iPhone13,
         ),
         InspectorAddon(),
@@ -73,27 +54,17 @@ class WidgetbookApp extends StatelessWidget {
         ),
         ThemeAddon(
           themes: [
-            WidgetbookTheme(
-              name: 'Light',
-              data: AppThemeData.light,
-            ),
-            WidgetbookTheme(
-              name: 'Dark',
-              data: AppThemeData.dark,
-            ),
+            WidgetbookTheme(name: 'Light', data: AppThemeData.light),
+            WidgetbookTheme(name: 'Dark', data: AppThemeData.dark),
           ],
-          themeBuilder: (context, theme, child) => AppTheme(
-            data: theme,
-            child: child,
-          ),
+          themeBuilder: (context, theme, child) =>
+              AppTheme(data: theme, child: child),
         ),
         AlignmentAddon(),
         TextScaleAddon(),
         BuilderAddon(
           name: 'SafeArea',
-          builder: (_, child) => SafeArea(
-            child: child,
-          ),
+          builder: (_, child) => SafeArea(child: child),
         ),
         SemanticsAddon(),
       ],
