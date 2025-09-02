9cf3d9df6c15e9ec0b747377daed5feb1a2aecbe
kumarpalsinh25 <info@dajiraj.com>
2025-09-02 14:43:04 +0530

    Border Redius Changes

--- lib/ui/widgets/primary_button.dart
+++ lib/ui/widgets/primary_button.dart
@@ -34,7 +34,7 @@ class PrimaryButton extends StatelessWidget {
                 ? AppTheme.of(context).background.primaryInactive
                 : AppTheme.of(context).background.brand,
             borderRadius: BorderRadius.circular(
-              AppTheme.of(context).radius.full,
+              AppTheme.of(context).radius.sm,
             ),
           ),
           child: Row(
